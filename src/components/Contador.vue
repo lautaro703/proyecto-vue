67fef340822695fd2b960baecfd7abd2bed4e277
Lautaro Vera <colo30036@gmail.com>
2025-05-26 00:55:58 -0300

    Crear componente de contar

---
+++ src/components/Contador.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <h1>Holaa</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">decrementar</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0)
+const incrementar = () => {
+  contador.value++;
+}
+const decrementar = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p {color: blue;}
+</style>
