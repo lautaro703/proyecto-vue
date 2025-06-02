3f22ac8e68b1bd0a206eaff51e49cfb3d0972daf
Lautaro Vera <colo30036@gmail.com>
2025-06-02 12:00:32 -0300

    carga de mayoria de cambios

---
+++ src/components/Siderbar.vue
@@ -0,0 +1,21 @@
+<template>
+ <div class="sidebar">
+  <h2>Barra Literal</h2>
+  <ul>
+  <li><router-link to="/contador">Contador</router-link></li>
+  <li><router-link to="/tarea">ListaDeTarea</router-link></li>
+  </ul>
+ </div>
+</template>
+
+<script setup>
+
+</script>   
+
+<style>
+ .sidebar {
+   width: 200px;
+   background-color: aqua;
+   padding: 20px;
+ }
+</style>
